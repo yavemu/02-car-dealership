f06811c16927b11f79df8979aacefbba2f381785
YAMid <yavemu@gmail.com>
2022-08-10 23:58:54 -0500

    resolve eslint error

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -3,16 +3,16 @@ import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
-  constructor(private readonly CarsService: CarsService) {}
+  constructor(private readonly carsService: CarsService) {}
 
   @Get()
   get() {
-    return this.CarsService.findAll();
+    return this.carsService.findAll();
   }
 
   @Get(':id')
   getById(@Param('id') id: string) {
-    const car = this.CarsService.findById(Number(id));
+    const car = this.carsService.findById(Number(id));
     return car;
   }
 }
