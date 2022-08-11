3a14620023ae56d88bf11220a3844a1829cbd9d2
YAMid <yavemu@gmail.com>
2022-08-10 23:27:09 -0500

    add car get method

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('cars')
-export class CarsController {}
+export class CarsController {
+  private cars = ['Toyota', 'Audi', 'Bmw'];
+
+  @Get()
+  get() {
+    return this.cars;
+  }
+}
