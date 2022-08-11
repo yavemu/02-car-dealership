13947b27892a47247cf65f99f99d11694f5c5408
YAMid <yavemu@gmail.com>
2022-08-11 08:54:47 -0500

    add other base methods in controller cars

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -14,5 +23,20 @@ export class CarsController {
   getById(@Param('id', ParseIntPipe) id: number) {
     const car = this.carsService.findById(id);
     return car;
+  }
+
+  @Post()
+  create(@Body() body: any) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id', ParseIntPipe) id: number, @Body() body: any) {
+    return { id, body };
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return id;
   }
 }
